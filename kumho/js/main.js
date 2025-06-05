093789a6cdef1922b06b427c3892cd73b71308ee
jujululu85 <jujululu85@gmail.com>
2025-06-05 10:24:33 +0900

    .

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -33,11 +33,10 @@ $(document).ready(function(){
 
     /**************** news swiper 연결 : 시작 *****************/
     const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        slidesPerView: auto, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
-                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            768: {    /* 640px 이상일때 적용 */
                 spaceBetween: 24,
             },
         },
@@ -48,13 +47,17 @@ $(document).ready(function(){
         //    disableOnInteraction: true,
         //},
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.news .ctrl_wrap .btn_next',
+            prevEl: '.news .ctrl_wrap .btn_prev',
         },
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            el: '.news .ctrl_wrap .count', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
             type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+        scrollbar: {
+            el: ".news .ctrl_wrap .scrollbar",
+            hide: false,
         },
     });
 
