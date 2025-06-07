d98c577b5debcf5034cc610f7f5e50993910474e
jujululu85 <jujululu85@gmail.com>
2025-06-07 23:16:21 +0900

    .

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -8,10 +8,6 @@ $(document).ready(function(){
         },
         effect: "fade", /* fade 효과 */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual .ctrl_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.visual .ctrl_wrap button.btn_prev',  
-        },
     });
     // visual_swiper.autoplay.stop();  /* 일시정지 기능 */
     // visual_swiper.autoplay.start();  /* 재생 기능 */
