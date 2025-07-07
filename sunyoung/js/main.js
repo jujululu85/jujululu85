f20881d0b3bc4b8c797337863ac58f419df02762
jujululu85 <jujululu85@gmail.com>
2025-07-07 22:56:31 +0900

    .

--- sunyoung/js/main.js
+++ sunyoung/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 25000,
+            delay: 5000,
             disableOnInteraction: true,
         },
 
