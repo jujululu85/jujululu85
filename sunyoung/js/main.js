f4ddc3c0272e353717d69d183ace6314a15cd9db
jujululu85 <jujululu85@gmail.com>
2025-07-07 18:03:04 +0900

    .

--- sunyoung/js/main.js
+++ sunyoung/js/main.js
@@ -1,47 +1,19 @@
 $(document).ready(function(){
-    AOS.init({
-        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
-        duration: 500, // 애니메이션 효과가 작동되는 시간
-        easing: 'ease', // 가속도
-    });
-
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 5000,
+            delay: 25000,
             disableOnInteraction: true,
         },
+
         effect: "fade", /* fade 효과 */
+
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    });
-    // visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-    // visual_swiper.autoplay.start();  /* 재생 기능 */
 
-    $('.visual .ctrl_wrap button.btn_stop').on('click', function(){
-        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-        //console.log('정지버튼 클릭!!!!!!!!')
-        $(this).hide()
-        $('.visual .ctrl_wrap button.btn_play').show()
-    })
-    $('.visual .ctrl_wrap button.btn_play').on('click', function(){
-        //console.log('재생버튼')
-        visual_swiper.autoplay.start();  /* 재생 기능 */
-        $(this).hide()
-        $('.visual .ctrl_wrap button.btn_stop').show()
-    })
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.visual .swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
 
-
-
-
-
-    $('.menu-open').on('mouseenter', function() {
-      if (window.innerWidth > 1024) {
-        $('.fullscreen-menu').fadeIn(300);
-      }
-    });
-    $('.hamburger').on('click', function() {
-      $('.fullscreen-menu').fadeIn(300);
-    });
-    $('.close-btn').on('click', function() {
-      $('.fullscreen-menu').fadeOut(300);
     });
 });
