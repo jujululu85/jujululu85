5b5915b41511d6f315487162186a0a1007c2fa5f
jujululu85 <jujululu85@gmail.com>
2025-07-08 23:04:11 +0900

    .

--- sunyoung/js/common.js
+++ sunyoung/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	/* 
     header .gnb .gnb_wrap ul.depth1 > li 마우스를 오버했을때
-    오버한 il에만 active 클래스가 들어가야함
+    오버한 li에만 active 클래스가 들어가야함
     */
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
 		$(this).addClass('active');	
