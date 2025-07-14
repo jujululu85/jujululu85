11df67e7f9e466fdb9b4ec91bebf5aa86b3a93ce
jujululu85 <jujululu85@gmail.com>
2025-07-14 18:02:24 +0900

    .

--- sunyoung/js/common.js
+++ sunyoung/js/common.js
@@ -1,19 +1,49 @@
 $(document).ready(function(){
-	/* 
-    header .gnb .gnb_wrap ul.depth1 > li 마우스를 오버했을때
-    오버한 li에만 active 클래스가 들어가야함
-    */
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
-		$(this).addClass('active');	
+	/*header .util .hamburger .hamburger_open를 클릭하면 fullscreen_menu에 open클래스를 줌
+    .fullscreen_menu .menu_close를 클릭하면 fullscreen_menu가 open클래스 삭제  */
+
+    $('header .util .hamburger .hamburger_open').on('click', function(){
+		$('.fullscreen_menu').addClass('open');
 	});
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
-		$(this).removeClass('active');	
+    $('.fullscreen_menu .menu_close').on('click', function(){
+		$('.fullscreen_menu').removeClass('open');
 	});
 
-  $('.quick .detail ul li.top button').on('click', function(){
-    //console.log('top버튼 눌렀다!!!')
-    $('html, body').animate({
-        scrollTop: 0
-    }, 500)
-})
-});
+    /* .fullscreen_menu .menu_wrap ul.depth1 > li > span를 클릭했을때
+        1. 상위 li에 open클래스가 있는경우 open클래스를 삭제
+        2. 상위 li에 open클래스가 없는경우 open클래스를 줌 */
+    $('.fullscreen_menu .menu_wrap ul.depth1 > li > span').on('click', function(){
+        if($(this).parent().hasClass('open') == true){
+            //console.log('열림')
+            $(this).parent().removeClass('open');
+        }else{
+            //console.log('닫혀있음')
+            $(this).parent().addClass('open');
+        }
+    });
+
+
+    /* 
+    header .gnb .gnb_wrap ul.depth1 > li 
+        마우스를 오버하면 active 클래스를 줌
+        마우스를 벚어나면 active 클래스를 삭제 
+    */
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        //console.log('오버했다')
+        ($(this).addClass('active'))
+    });
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+        //console.log('오버아웃')
+        ($(this).removeClass('active'))
+    });
+
+    /*
+    .quick .detail ul li.top button 클릭하면 상단으로 스크롤됨 
+    */
+    $('.quick .detail ul li.top button').on('click', function(){
+		$('html, body').animate({
+            scrollTop : 0
+          }, 500);
+	});
+});//$(document).ready
