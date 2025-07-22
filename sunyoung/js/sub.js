772850f2e510e15edae59eb9ec6eadce366b90f0
jujululu85 <jujululu85@gmail.com>
2025-07-22 22:50:07 +0900

    .

--- sunyoung/js/sub.js
+++ sunyoung/js/sub.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
 		$('.ctn_biz .biz_list ul li').removeClass('active');
 		$(this).addClass('active');
 	});
+
+     /* 
+     .ctn_design .top 클릭하면 상단으로 스크롤됨 
+    */
+    $('.ctn_design .top').on('click', function(){
+		$('html, body').animate({
+            scrollTop : 0
+          }, 500);
+	});
 })//$(document).ready
